36154d32a5725b13f6f9e4855cdd78db0cc6e094
soma_arc <somaarcr@gmail.com>
2015-05-14 10:48:44 +0900

    マウスによるパラメータ操作を実装 close #2

--- src/ui/Display.java
+++ src/ui/Display.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -15,6 +16,12 @@ import fuchs.ComplexProbability;
 
 public class Display extends JPanel{
 	private ComplexProbability cp, cp2;
+
+	private double magnification = 500;
+	private boolean isDraggingQ = false;
+	private boolean isDraggingR = false;
+	//private Complex draggingPoint = null;
+
 	public Display(){
 		Complex a1 = new Complex(0.3, 0.4);
 		Complex a2 = new Complex(0.3, 0.1);
@@ -22,25 +29,58 @@ public class Display extends JPanel{
 		cp.setColor(Color.red);
 		cp2 = cp.replace(1);
 		cp2.setColor(Color.green);
+
+		addMouseListener(new MousePressedAdapter());
+		addMouseMotionListener(new MouseDraggedAdapter());
 	}
-	
-	private double magnification = 500;
+
 	public void paintComponent(Graphics g){
 		Graphics2D g2 = (Graphics2D) g;
 		g.setColor(Color.black);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		drawAxis(g2);
-		//cp.draw(g2, magnification, getWidth(), getHeight());
-		cp2.draw(g2, magnification, getWidth(), getHeight());
+		cp.draw(g2, magnification, getWidth(), getHeight());
+		cp.drawControlPoints(g2, magnification, getWidth(), getHeight());
 	}
-	
+
 	private void drawAxis(Graphics2D g2){
 		g2.setColor(Color.gray);
 		g2.drawLine(0, getHeight()/2, getWidth(), getHeight()/2); //x axis
 		int n = (int)(getWidth() / 2 / (1 * magnification)); 
 		for(int i = -n ; i <= n ; i++){
 			g2.drawLine((int) magnification * i + getWidth()/2, 0, (int) magnification * i + getWidth()/2, getHeight());
+		}
+	}
+	
+	private class MousePressedAdapter extends MouseAdapter{
+		public void mousePressed(MouseEvent e){
+			double mouseX = e.getX() - getWidth() / 2;
+			double mouseY = e.getY() - getHeight() / 2;
+			if(cp.isClickedQ(mouseX , mouseY, magnification)){
+				isDraggingQ = true;
+			}else if(cp.isClickedR(mouseX, mouseY, magnification)){
+				isDraggingR = true;
+			}
+		}
+		
+		public void mouseReleased(MouseEvent e){
+			isDraggingQ = false;
+			isDraggingR = false;
+		}
+	}
+	
+	private class MouseDraggedAdapter extends MouseMotionAdapter{
+		public void mouseDragged(MouseEvent e){
+			double mouseX = e.getX() - getWidth() / 2;
+			double mouseY = e.getY() - getHeight() / 2;
+			Complex np = new Complex(mouseX/magnification, mouseY / magnification);
+			if(isDraggingQ){
+				cp.moveQ(np);
+			}else if(isDraggingR){
+				cp.moveR(np);
+			}
+			repaint();
 		}
 	}
 }
