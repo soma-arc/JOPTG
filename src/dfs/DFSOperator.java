8e35819dfc33047c28205b26aca9181326f7e52a
soma_arc <somaarcr@gmail.com>
2015-05-19 12:43:46 +0900

    線で結べるような語の探索順に変更し、点の間隔が閾値以下になった時に探索を打ち切るように変更 #5

--- src/dfs/DFSOperator.java
+++ src/dfs/DFSOperator.java
@@ -19,6 +19,7 @@ public class DFSOperator {
 		fixPoints = new Complex[gens.length];
 		for(int i = 0 ; i < gens.length ; i++){
 			this.gens[i] = gens[i];
+			fixPoints[i] = Mobius.mobiusOnPoint(gens[i], Complex.INFINITY);
 		}
 	}
 
@@ -29,6 +30,10 @@ public class DFSOperator {
 		tags[0] = -1;
 		level = 0;
 		pointsList = new ArrayList<>();
+
+		words[1] = gens[1];
+		tags[1] = 1;
+		level++;
 	}
 	
 	public ArrayList<Complex> run(int maxLevel, double epsilon){
@@ -47,18 +52,14 @@ public class DFSOperator {
 	
 	private void goForward(){
 		level++;
-		if(tags[level -1] == 0){
-			tags[level] = 1;
-		}else{
-			tags[level] = 0;
-		}
+		tags[level] = (tags[level-1] + 1) % 3; 
 
 		words[level] = words[level -1].mult(gens[tags[level]]);
 	}
 
 	private boolean isAvailableTurn(){
-		if(tags[level + 1] >= 2 || 
-		   (tags[level] == 2 && tags[level + 1] == 1)){
+		if((tags[level] + 2) % 3 != (tags[level + 1] + 1) % 3 ||
+		   (level == 2 && tags[1] == 2 && tags[2] == 1 && tags[3] == 2)){
 			return false;
 		}
 		return true;
@@ -70,22 +71,18 @@ public class DFSOperator {
 	
 	private void turnAndGoForward(){
 		tags[level + 1] = (tags[level + 1] + 1)%3;
-		if(tags[level] == tags[level + 1]){
-			tags[level + 1] = (tags[level + 1] + 1)%3;
-		}
 
 		words[level + 1] = words[level].mult(gens[tags[level + 1]]);
 		level++;
 	}
 
 	private boolean branchTermination(int maxLevel, double epsilon){
-		if(level == maxLevel){
-			if((tags[1] == 2 && tags[2] == 1 && tags[3] == 0) ||
-			   tags[1] == 0){
-				return true;
-			}
-			pointsList.add(Mobius.mobiusOnPoint(words[level], Complex.INFINITY));
-			
+		Complex[] z = {Mobius.mobiusOnPoint(words[level], fixPoints[(tags[level] + 1) % 3]),
+					   Mobius.mobiusOnPoint(words[level], fixPoints[(tags[level] + 2) % 3])};
+		
+		if((z[0].dist(z[1])) < epsilon || level == maxLevel){
+			pointsList.add(z[0]);
+			pointsList.add(z[1]);
 			return true;
 		}
 		
